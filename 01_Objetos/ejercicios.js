a7af9d9024671dc0ce2052c62639eefa7c48f921
Pedro A. González <pgonzalez@post.com>
2020-11-19 22:38:28 -0500

    Agregar ejercicios

---
+++ 01_Objetos/ejercicios.js
@@ -0,0 +1,16 @@
+console.log('Hola desde ejercicios!');
+
+// 1.- Haz una clase llamada Persona que siga las siguientes condiciones:
+// 	 Sus atributos son: nombre, edad, RFC, sexo, peso y altura.
+// 	 calcularIMC()
+// 	 esMayorDeEdad()
+// 	 El constructor pide nombre, edad,sexo,peso y altura
+// 	 Generar el RFC a partir de el nombre, edad y sexo
+
+
+// 2.- Crear una clase Cuenta que tenga los siguientes atributos y metodos:
+// 	 -Titular y cantidad
+// 	 -ingresar(cantidad)
+// 	 -retirar(cantidad)
+// 	 Hacer las validaciones previas
+// 	 Como regla de negocio no debe de tener más de $900 y menos de $10
