d33e650ed093cef4be200fbd002e94d7e315b516
Pedro A. González <pgonzalez@post.com>
2020-11-20 23:16:58 -0500

    Agregar ejercicios de herencia

---
+++ 02_Herencia/ejercicios.js
@@ -0,0 +1,43 @@
+console.log('Hola desde ejercicios!');
+
+// EJERCICIO HERENCIA
+// Clase Largometraje (atributos: nombre y duracion)
+//   Subclases: Pelicula (atributos: genre)/ Documental (atributos: topic)
+// Clase Cine
+//   metodo: reproducir(largometraje) {
+//     return ‘Wall-E tiene una duración de 103 minutos’;
+//   }
+// El código debe lucir similar a esto:
+// Cine.reproducir(pelicula);
+// Cine.reproducir(documental);
+
+
+// -----------------------------------------------------------------------------------------------------------
+// 1. Hacer superclase Maestro y subclases Maestro de Física y Maestro de Música:
+//        a. A cada uno asignarle su atributo materia y calificaciones
+//        b. Calcular el promedio de grupo a partir de calificaciones (puedes usar arreglos)
+//        c. El maestro de física tiene un atributo "antiguedad" que guarda un valor numerico
+//        d. El maestro de música tiene un atributo "edad" también guardando un valor numérico.
+// -----------------------------------------------------------------------------------------------------------
+
+
+// -----------------------------------------------------------------------------------------------------------
+// 2.- Crea una superclase llamada Electrodoméstico con las siguientes características:
+//        a. Sus atributos son precio, color, consumoEnergetico y capacidad (peso máximo)
+//        b. El constructor solo debe pedir precio, color y capacidad.
+//        c. consumoEnergetico debe iniciar con valor de 100
+//     Crea una subclase de Electrodomestico llamada Lavadora con las siguientes características:
+//        a. Su atributo es carga(kg de ropa), además de los atributos heredados.
+//        b. Crea el método precioFinal(). Este se calcula multiplicando el consumoEnergetico por la carga.
+// -----------------------------------------------------------------------------------------------------------
+
+
+// -----------------------------------------------------------------------------------------------------------
+// 3.- Crear una superclase Bebida que herede a dos subclases Cerveza y Refresco
+//        a. Ambas clases deben tener la propiedad cantidad (ml)
+//        b. La clase Refresco debe tener el atributo azucar(g)
+//        c. La clase Cerveza debe el atributo porcentajeAlcohol.
+//        d. Crear los getters y setters correspondientes.
+//           GET -> OBTENER O MOSTRAR EL VALOR DE UNA PROPIEDAD
+//           SET -> CAMBIAR O ACTUALIZAR EL DE VALOR DE UNA PROPIEDAD
+// -----------------------------------------------------------------------------------------------------------
