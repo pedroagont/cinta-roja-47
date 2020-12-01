87b9571e5c37c52bad58a98bbf9f18948407c733
Pedro A. González <pgonzalez@post.com>
2020-12-01 03:04:15 -0500

    Agregar ejercicio promesas

---
+++ 06_Promesas/ejercicio.js
@@ -0,0 +1,15 @@
+// EJERCICIO CRUD DE PETICIONES CON PROMESAS
+//
+// Convierte todas tus funciones del ejercicio del
+// CRUD de Cliente para que ahora devuelvan promesas.
+//
+// Es necesario que personalicen el mensaje de error
+// de cada de una de las funciones para que sea más
+// fácil identificar dónde se rompe nuestro código.
+//
+// Finalmente para probar tus nuevas promesas, tienes
+// que hacer los siguientes pasos encadenando promesas:
+// 1. Crear un autor
+// 2. Modificar el autor
+// 3. Obtener ese autor
+// 4. Eliminar el autor
