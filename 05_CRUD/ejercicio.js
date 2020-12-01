829d6a6067f724d4d1c1cfff75a5ed10b44a0dee
Pedro A. González <pgonzalez@post.com>
2020-12-01 02:48:42 -0500

    Agregar ejercicio CRUD

---
+++ 05_CRUD/ejercicio.js
@@ -0,0 +1,7 @@
+// EJERCICIO
+// Con el API: https://goodreads-devf-aaron.herokuapp.com/api/v1/authors/
+// Ejecutar el siguiente ciclo de vida de un autor:
+// Crear un autor LISTO
+// Modificar ese autor LISTO
+// Obtener autor LISTO
+// Borrar autor
